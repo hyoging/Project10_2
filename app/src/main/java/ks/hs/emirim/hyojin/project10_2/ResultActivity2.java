4b54ca53d8c509c85e197bd638fd2f360b808743
User <w2030@e-mirim.hs.kr>
2021-07-21 10:16:33 +0900

    Init Change ViewFlipper Commit

---
+++ app/src/main/java/ks/hs/emirim/hyojin/project10_2/ResultActivity2.java
@@ -0,0 +1,14 @@
+package ks.hs.emirim.hyojin.project10_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result2);
+    }
+}
